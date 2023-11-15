34fdd64f16c4f6f73a2399120dcc0e20b12c20e2
Egils <63558651+looppng@users.noreply.github.com>
2023-11-15 10:56:40 +0200

    4-6

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,20 +1,25 @@
+// 1.
 
 document.getElementById('btn1').onclick = function(){
     document.getElementById('div1').style.backgroundColor = 'Yellow';
 }
 
+// 2.
+
 document.getElementById('btn2').onclick = function(){
     document.getElementById('div2').innerText = 'Success';
 }
+
+// 3.
 
 document.getElementById('btn3').onclick = function(){
     document.getElementById('div3').style.display = 'none';
 }
 
+// 4.
 
-
-var switchButton = document.getElementById('btn4');
-var div4 = document.getElementById('div4');
+const switchButton = document.getElementById('btn4');
+const div4 = document.getElementById('div4');
 
 var isNone = false;
 const changeDisplay = () => {
@@ -26,3 +31,52 @@ const changeDisplay = () => {
     isNone =!isNone;
 }
 switchButton.onclick = changeDisplay;
+
+// 5.
+
+const switchColors = document.getElementById('btn5');
+const div5 = document.getElementById('div5');
+
+var colors = ['Red', 'Green', 'Blue', 'Magenta', 'Yellow'];
+
+const getColor = () => {
+    var randomColor = Math.floor(Math.random() * colors.length);
+    return colors[randomColor];
+}
+
+const changeColor = () => {
+    var randomColor = getColor();
+    div5.style.backgroundColor = randomColor;
+    switchColors.style.backgroundColor = randomColor;
+}
+
+switchColors.onclick = changeColor;
+
+//6.
+
+const counter = document.getElementById('div6');
+const intervalButton = document.getElementById('btn6');
+
+var count = 0;
+var interval: NodeJS.Timeout;
+
+function updateCounter() {
+    count++;
+    if (count > 10) {
+        count = 0;
+    }
+    counter.innerText = count.toString();
+}
+
+intervalButton.onclick = () => {
+    interval = setInterval(updateCounter, 3000);
+}
+
+//7.
+
+
+
+
+
+
+
